eb6ef8a174a7eac3ccd771f17741528aa74306ab
MattuW4 <>
2023-08-12 15:46:47 +0000

    Add while loop to enclose conditions for game

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 from random import randint
 
-
-
-
 def start_game():
     
     print("/" + "-" *80 + "/")
@@ -11,19 +8,19 @@ def start_game():
     print("Enter a row and column coordinate choice to try and sink the computer battleship.")
     print("/" + "-" *80 + "/")
     captain_name = input("Please enter your name, Captain: \n")
-    
 
 start_game()
 
-board = []
+score = 0
 
+board = []
 def print_board(board):
     for i in board:
         print(" ".join(i))
 
     for i in range(0, 5):
         board.append(["0"]*5)
-            
+         
 print_board(board)
 
 def row_random(board):
@@ -35,19 +32,21 @@ def column_random(board):
 ship_row = row_random(board)
 ship_column = column_random(board)
 
-select_row = int(input("Select a row: "))
-select_column = int(input("Select a column: "))
 
-print(select_row)
-print(select_column)
-
-if select_row == ship_row and select_column == ship_column:
-    print("Hit! You sank an enemy battleship!")
-elif (select_row > 5 or select_row < 0) or (select_column > 5 or select_column < 0):
+while score < 3:
+    select_row = int(input("Select a row: "))
+    select_column = int(input("Select a column: "))
+    print(select_row)
+    print(select_column)
+    if select_row == ship_row and select_column == ship_column:
+        score += 1
+        print("Hit! You sank an enemy battleship!")
+        print(score)
+    elif (select_row > 5 or select_row < 0) or (select_column > 5 or select_column < 0):
         print("Out of range! Try again...")
-elif board[select_row][select_column] == "X":
+    elif board[select_row][select_column] == "X":
         print("You've already tried these coordinates... Fire again!")
-else:
-    print("You missed. Try again...")
-    board[select_row][select_column] ="X"
-    print_board(board)
+    else:
+        print("You missed. Try again...")
+        board[select_row][select_column] = "X"
+        print_board(board)
