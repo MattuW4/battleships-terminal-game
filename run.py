556761b1225129e6751e3e2b4a6277f71db536a1
MattuW4 <>
2023-08-12 13:30:05 +0000

    Initial commit. Add  welcome load out and name input.

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def start_game():
+    print("/" + "-" *80 + "/")
+    print("Welcome to this terminal version of Battleships :D")
+    print("Board size: 4 x 4. Total number of ships: 3")
+    print("Enter a row and column coordinate choice to try and sink the computer battleship.")
+    print("/" + "-" *80 + "/")
+    captain_name = input("Please enter your name, Captain: \n")
+
+start_game()
