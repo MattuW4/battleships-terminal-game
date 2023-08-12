68e68d444ed50b5da5d105962ea2dca292a115f6
MattuW4 <>
2023-08-12 14:11:30 +0000

    Add function to create board. Add function to add ship to random row & column. Add elseIf loop for hit or miss output as well as update board.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def start_game():
     print("/" + "-" *80 + "/")
     print("Welcome to this terminal version of Battleships :D")
@@ -5,5 +7,40 @@ def start_game():
     print("Enter a row and column coordinate choice to try and sink the computer battleship.")
     print("/" + "-" *80 + "/")
     captain_name = input("Please enter your name, Captain: \n")
+    
 
 start_game()
+
+board = []
+
+for i in range(0, 5):
+    board.append(["0"]*5)
+
+
+def print_board(board):
+    for i in board:
+        print(i)
+        
+print_board(board)
+
+def row_random(board):
+    return randint(0, len(board)-1)
+    
+def column_random(board):
+    return randint(0, len(board)-1)
+
+ship_row = row_random(board)
+ship_column = column_random(board)
+
+select_row = int(input("Select a row: "))
+select_column = int(input("Select a column: "))
+
+print(select_row)
+print(select_column)
+
+if select_row == ship_row and select_column == ship_column:
+    print("Hit! You sank the enemy battleship!")
+else:
+    print("You missed. Try again")
+    board[select_row][select_column]="X"
+    print_board(board)
