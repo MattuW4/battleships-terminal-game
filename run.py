751ef7dff2bef2de74368b879856be7fba8077bd
MattuW4 <>
2023-08-12 15:26:28 +0000

    Add elseIf statements to check choices already made and check choices within range of board

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from random import randint
 
+
+
+
 def start_game():
+    
     print("/" + "-" *80 + "/")
     print("Welcome to this terminal version of Battleships :D")
     print("Board size: 4 x 4. Total number of ships: 3")
@@ -13,14 +17,13 @@ start_game()
 
 board = []
 
-for i in range(0, 5):
-    board.append(["0"]*5)
-
-
 def print_board(board):
     for i in board:
-        print(i)
-        
+        print(" ".join(i))
+
+    for i in range(0, 5):
+        board.append(["0"]*5)
+            
 print_board(board)
 
 def row_random(board):
@@ -39,8 +42,12 @@ print(select_row)
 print(select_column)
 
 if select_row == ship_row and select_column == ship_column:
-    print("Hit! You sank the enemy battleship!")
+    print("Hit! You sank an enemy battleship!")
+elif (select_row > 5 or select_row < 0) or (select_column > 5 or select_column < 0):
+        print("Out of range! Try again...")
+elif board[select_row][select_column] == "X":
+        print("You've already tried these coordinates... Fire again!")
 else:
-    print("You missed. Try again")
-    board[select_row][select_column]="X"
+    print("You missed. Try again...")
+    board[select_row][select_column] ="X"
     print_board(board)
