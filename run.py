37c827b0aafeca16e21de10404c288a1a11065a5
MattuW4 <>
2023-08-12 18:12:58 +0000

    Update while loop to print rows, columns and ships hit

--- run.py
+++ run.py
@@ -1,56 +1,48 @@
 from random import randint
 
-def start_game():
-    print("/" + "-" *80 + "/")
-    print("Welcome to this terminal version of Battleships :D")
-    print("Board size: 4 x 4. Total number of ships: 3")
-    print("Enter a row and column coordinate choice to try and sink the computer battleship.")
-    print("/" + "-" *80 + "/")
-    captain_name = input("Please enter your name, Captain: \n")
-
-start_game()
-
-score = 0
 
 board = []
 
+for i in range(0, 5):
+        board.append(["0"]*5)
+
 def print_board(board):
     for i in board:
-        print(" ".join(i))
-
-    for i in range(0, 5):
-        board.append(["0"]*5)
+        print(" ".join(i))   
          
 print_board(board)
 
 def row_random(board):
     return randint(0, len(board)-1)
-    
+
 def column_random(board):
     return randint(0, len(board)-1)
 
 ship_row = row_random(board)
 ship_column = column_random(board)
+select_row = int(input("Select a row: "))
+select_column = int(input("Select a column: "))
 
+ships_remaining = 0
 
-while score < 3:
-    print(score)
+while ships_remaining < 3:
+    print(ships_remaining)
+    print(ship_row)
+    print(ship_column)
     select_row = int(input("Select a row: "))
-    select_column = int(input("Select a column: "))
-    print(select_row)
-    print(select_column)
+    select_column = int(input("Select a column: "))    
+
     if select_row == ship_row and select_column == ship_column:
-        score += 1        
-        print("Hit! You sank an enemy battleship!")
-        board[select_row][select_column] = "@"
+        ships_remaining += 1
+        print("Direct hit! You sunk an enemy battleship.")
+        board[select_row][select_column] = "#"
         print_board(board)
-        
-    elif (select_row > 5 or select_row < 0) or (select_column > 5 or select_column < 0):
-        print("Out of range! Try again...")
-    elif board[select_row][select_column] == "X":
-        print("You've already tried these coordinates... Fire again!")
     else:
-        print("You missed. Try again...")
-        print(score)
-        board[select_row][select_column] = "X"
-        print_board(board)
+        if (select_row < 0 or select_column > 4) or (select_row < 0 or select_column > 4):
+            print("That's not on the board! Try again...")
+        elif (board[select_row][select_column] == "X"):
+            print("You've already tried that location! Try again...")
+        else:
+            print("That's a miss! Try again...")
+            board[select_row][select_column] = "X"
+            print_board(board)
